b491e7e34833cee43e142afa026159c6d63a603e
Craig Martin <cmart1220@gmail.com>
2017-08-08 09:30:04 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
 const app = express();
+const mongoose = require('mongoose');
 
-app.get('/api', (req, res) => {
-  res.send({ hi: 'there' });
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber', {
+    useMongoClient: true
+  });
+}
+
+app.use(bodyParser.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
 });
 
 module.exports = app;
